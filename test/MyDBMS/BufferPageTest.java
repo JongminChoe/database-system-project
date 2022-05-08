e25c6aff0189463c1a36468263c7c90e39b9c77e
JongminChoe <aaaa961224@gmail.com>
2022-05-08 20:38:14 +0900

    refactor BufferPage test cases
    add order of tests
    add BeforeAll method to clean previous test result
    add more test cases

--- test/MyDBMS/BufferPageTest.java
+++ test/MyDBMS/BufferPageTest.java
@@ -1,16 +1,27 @@
 package MyDBMS;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BufferPageTest {
 
+    @BeforeAll
+    static void BeforeAll() {
+        File file = new File("TestPage");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
     @Test
+    @Order(1)
     void testPageMustBeDirtyWhenPayloadWasPassedToTheConstructor() {
         byte[] payload = new byte[BufferPage.PAGE_SIZE];
         BufferPage page = new BufferPage("TestPage", 0, payload);
@@ -19,11 +30,19 @@ class BufferPageTest {
     }
 
     @Test
+    @Order(2)
+    void testInvalidPayloadSizeInConstructor() {
+        assertThrows(AssertionError.class, () -> new BufferPage("TestPage", 0, new byte[BufferPage.PAGE_SIZE + 1]));
+    }
+
+    @Test
+    @Order(3)
     void testPageFromNonExistentFile() {
         assertThrows(FileNotFoundException.class, () -> new BufferPage("NonExistentFileName", 0));
     }
 
     @Test
+    @Order(4)
     void testPayloadEqualsOnIndex0() {
         // dummy payload
         byte[] payload = new byte[BufferPage.PAGE_SIZE];
@@ -44,6 +63,7 @@ class BufferPageTest {
     }
 
     @Test
+    @Order(5)
     void testPayloadEqualsOnIndex1() {
         // dummy payload
         byte[] payload = new byte[BufferPage.PAGE_SIZE];
@@ -61,5 +81,11 @@ class BufferPageTest {
         }
         assert newPage != null;
         assertArrayEquals(payload, newPage.getPayload());
+    }
+
+    @Test
+    @Order(6)
+    void testIndexOutOfFileBound() {
+        assertThrows(IndexOutOfBoundsException.class, () -> new BufferPage("TestPage", 2));
     }
 }
