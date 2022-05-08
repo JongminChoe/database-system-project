47be8bc4ae202e80d2b7f7e62f7fd1e1458f378d
JongminChoe <aaaa961224@gmail.com>
2022-05-08 23:12:33 +0900

    add equals method to the Table class
    comparison required in Record::equals

--- src/MyDBMS/Table.java
+++ src/MyDBMS/Table.java
@@ -1,5 +1,7 @@
 package MyDBMS;
 
+import java.util.Objects;
+
 public class Table {
     private final String tableName;
     private final Column[] columns;
@@ -15,5 +17,17 @@ public class Table {
 
     public Column[] getColumns() {
         return this.columns;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Table table)) return false;
+        return Objects.equals(this.getTableName(), table.getTableName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getTableName());
     }
 }
