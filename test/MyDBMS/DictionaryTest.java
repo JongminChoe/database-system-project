8affbfed808428c5e882016688aa2756c7114437
JongminChoe <aaaa961224@gmail.com>
2022-05-23 01:14:31 +0900

    add test cases for Dictionary class
    test creating duplicate table name

--- test/MyDBMS/DictionaryTest.java
+++ test/MyDBMS/DictionaryTest.java
@@ -1,10 +1,17 @@
 package MyDBMS;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DictionaryTest {
+
+    @BeforeEach
+    void BeforeEach() {
+        BufferManager.getInstance().forceFlush();
+        Dictionary.getInstance().reload();
+    }
 
     @Test
     void testNotExistentTable() {
@@ -27,5 +34,11 @@ class DictionaryTest {
                 new Column(Column.DataType.CHAR, "primary_key", 16)
         }, "primary_key"), Dictionary.getInstance().getTable("test_table"));
         assertNotNull(Dictionary.getInstance().getTable(Dictionary.TABLE_DICTIONARY).find("test_table"));
+    }
+
+    @Test
+    void testCreateDuplicateTable() {
+        assertDoesNotThrow(() -> Dictionary.getInstance().createTable("test_table", new Column[0], null));
+        assertThrows(IllegalArgumentException.class, () -> Dictionary.getInstance().createTable("test_table", new Column[0], null));
     }
 }
