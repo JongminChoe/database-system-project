3ca9080018d813f1946d9f8ae300f078168743fa
JongminChoe <aaaa961224@gmail.com>
2022-05-22 16:39:24 +0900

    add Record addition and filter methods to the Table class

--- src/MyDBMS/Table.java
+++ src/MyDBMS/Table.java
@@ -57,6 +57,77 @@ public class Table {
                 .flatMap(page -> new SlottedPage(this, page.getPayload()).getRecords().stream());
     }
 
+    public void addRecord(Record record) {
+        if (this.getPrimaryColumn() != null) {
+            Object primaryKey = switch (this.columns.get(this.getPrimaryColumn()).getType()) {
+                case CHAR -> record.getChar(this.getPrimaryColumn());
+                case VARCHAR -> record.getVarchar(this.getPrimaryColumn());
+            };
+            if (this.find(primaryKey) != null) {
+                return;
+            }
+        }
+
+        for (int i = 0; ; i++) {
+            BufferPage bufferPage;
+            try {
+                bufferPage = BufferManager.getInstance().getPage(this.getTableName(), i);
+            } catch (IOException e) {
+                // end of file
+                try {
+                    bufferPage = BufferManager.getInstance().getEmptyPage(this.getTableName(), i);
+                } catch (IOException ex) {
+                    // unknown error
+                    break;
+                }
+            }
+            SlottedPage slottedPage = new SlottedPage(this, bufferPage.getPayload());
+            try {
+                slottedPage.addRecord(record);
+            } catch (Exception e) {
+                // page full
+                continue;
+            }
+            bufferPage.setPayload(slottedPage.toByteArray());
+            break;
+        }
+    }
+
+    public Record find(Object value) {
+        if (this.getPrimaryColumn() == null) {
+            return null;
+        }
+
+        Record[] records = switch (this.columns.get(this.getPrimaryColumn()).getType()) {
+            case CHAR -> this.whereChar(this.getPrimaryColumn(), (String) value);
+            case VARCHAR -> this.whereVarchar(this.getPrimaryColumn(), (String) value);
+        };
+
+        return records.length > 0 ? records[0] : null;
+    }
+
+    public Record[] whereChar(String column, String value) {
+        if (!this.columns.containsKey(column)) {
+            throw new IllegalArgumentException("Column [" + column + "] does not exists");
+        }
+        if (this.columns.get(column).getType() != Column.DataType.CHAR) {
+            throw new IllegalArgumentException("Column [" + column + "] is not CHAR type");
+        }
+
+        return this.getAllRecords().filter(record -> record.getChar(column).equals(value)).toArray(Record[]::new);
+    }
+
+    public Record[] whereVarchar(String column, String value) {
+        if (!this.columns.containsKey(column)) {
+            throw new IllegalArgumentException("Column [" + column + "] does not exists");
+        }
+        if (this.columns.get(column).getType() != Column.DataType.VARCHAR) {
+            throw new IllegalArgumentException("Column [" + column + "] is not CHAR type");
+        }
+
+        return this.getAllRecords().filter(record -> record.getVarchar(column).equals(value)).toArray(Record[]::new);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- test/MyDBMS/TableTest.java
+++ test/MyDBMS/TableTest.java
@@ -58,28 +58,103 @@ class TableTest {
     }
 
     @Test
-    void testGetAllRecordsFromEmptyBufferPage() {
+    void testEmptyTable() {
         Table table = new Table("test_table", new Column[0]);
 
         assertEquals(0, table.getAllRecords().count());
     }
 
     @Test
-    void testGetAllRecordsFromSingleRecordBufferPage() {
+    void testAddSingleRecord() {
         Table table = new Table("test_table", new Column[]{
                 new Column(Column.DataType.CHAR, "test", 16)
         });
-        SlottedPage page = new SlottedPage(table);
-
         Record record = new Record(table);
         record.setChar("test", "hello world");
 
-        page.addRecord(record);
-
-        assertDoesNotThrow(() -> BufferManager.getInstance().getEmptyPage(table.getTableName(), 0).setPayload(page.toByteArray()));
+        table.addRecord(record);
 
         Record[] records = table.getAllRecords().toArray(Record[]::new);
-        assertEquals(1, records.length);
-        assertEquals(record, records[0]);
+        assertArrayEquals(new Record[]{record}, records);
+    }
+
+    @Test
+    void testAddDuplicatePrimaryKey() {
+        Table table = new Table("test_table", new Column[]{
+                new Column(Column.DataType.VARCHAR, "primary_key", 16)
+        }, "primary_key");
+        Record record = new Record(table);
+        record.setVarchar("primary_key", "hello world");
+
+        table.addRecord(record);
+        table.addRecord(record);
+
+        Record[] records = table.getAllRecords().toArray(Record[]::new);
+        assertArrayEquals(new Record[]{record}, records);
+    }
+
+    @Test
+    void testAddMultipleRecords() {
+        Table table = new Table("test_table", new Column[]{
+                new Column(Column.DataType.CHAR, "test", 16)
+        });
+        Record record1 = new Record(table).setChar("test", "hello world1");
+        Record record2 = new Record(table).setChar("test", "hello world2");
+
+        table.addRecord(record1);
+        table.addRecord(record2);
+
+        Record[] records = table.getAllRecords().toArray(Record[]::new);
+        assertArrayEquals(new Record[]{record1, record2}, records);
+    }
+
+    @Test
+    void testWhereChar() {
+        Table table = new Table("test_table", new Column[]{
+                new Column(Column.DataType.CHAR, "char_column", 16),
+                new Column(Column.DataType.VARCHAR, "varchar_column", 16)
+        });
+        Record record1 = new Record(table).setChar("char_column", "char1").setVarchar("varchar_column", "varchar1");
+        Record record2 = new Record(table).setChar("char_column", "char2").setVarchar("varchar_column", "varchar2");
+
+        table.addRecord(record1);
+        table.addRecord(record2);
+
+        assertArrayEquals(new Record[]{record1}, table.whereChar("char_column", "char1           "));
+        assertArrayEquals(new Record[]{record2}, table.whereChar("char_column", "char2           "));
+        assertArrayEquals(new Record[]{}, table.whereChar("char_column", "char3           "));
+    }
+
+    @Test
+    void testWhereVarchar() {
+        Table table = new Table("test_table", new Column[]{
+                new Column(Column.DataType.CHAR, "char_column", 16),
+                new Column(Column.DataType.VARCHAR, "varchar_column", 16)
+        });
+        Record record1 = new Record(table).setChar("char_column", "char1").setVarchar("varchar_column", "varchar1");
+        Record record2 = new Record(table).setChar("char_column", "char2").setVarchar("varchar_column", "varchar2");
+
+        table.addRecord(record1);
+        table.addRecord(record2);
+
+        assertArrayEquals(new Record[]{record1}, table.whereVarchar("varchar_column", "varchar1"));
+        assertArrayEquals(new Record[]{record2}, table.whereVarchar("varchar_column", "varchar2"));
+        assertArrayEquals(new Record[]{}, table.whereVarchar("varchar_column", "varchar3"));
+    }
+
+    @Test
+    void testFindRecord() {
+        Table table = new Table("test_table", new Column[]{
+                new Column(Column.DataType.VARCHAR, "primary_key", 16)
+        }, "primary_key");
+        Record record1 = new Record(table).setVarchar("primary_key", "key1");
+        Record record2 = new Record(table).setVarchar("primary_key", "key2");
+
+        table.addRecord(record1);
+        table.addRecord(record2);
+
+        assertEquals(record1, table.find("key1"));
+        assertEquals(record2, table.find("key2"));
+        assertNull(table.find("key3"));
     }
 }
